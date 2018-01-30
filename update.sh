0f6d3a856ba2109afbbc8fb0b9cab75afdc1eba6
Mayexia <350750076@qq.com>
2018-01-30 23:12:17 +0800

    添加内容

--- update.sh
+++ update.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/bash
 comment=添加内容
 path_name=Mayexia.github.io
+echo '更新源代码到github'
 git add .
 git commit -m "${comment}"
 git push -u origin master
+
+echo 'gitbook生成html内容'
 gitbook build
+
+echo '更新gitpages内容'
 rm -rf ../${path_name}/*
+cp -rf _book/* ../${path}/.
+cd ../${path_name}
+git add .
+git commit -m "${comment}"
+git push -u origin master
